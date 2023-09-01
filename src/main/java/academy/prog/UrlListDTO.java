9d9c318c8a522a91445d79fd48f73c1c262d0015
VikaKhaustova <haustovavika@gmail.com>
2023-09-02 01:20:44 +0300

    Initial commit

---
+++ src/main/java/academy/prog/UrlListDTO.java
@@ -0,0 +1,15 @@
+package academy.prog;
+
+import java.util.List;
+
+public class UrlListDTO {
+    private List<String> urls;
+
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    public void setUrls(List<String> urls) {
+        this.urls = urls;
+    }
+}
